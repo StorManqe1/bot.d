07d817fe768d2edabaf1285bf449d9f4a8d5514e
Glitch (v12-kayit-altyapi) <none>
2021-05-02 19:57:41 +0000

    🐈💅 Checkpoint ./bot.js:19497527/18

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('ready', async () => {
     client.appInfo = await client.fetchApplication();
   }, 600);
   
- client.user.setActivity(`SanCaK6 ❤️ Senpaii'q7`, { type:'WATCHING' })
+ client.user.setActivity(`SanCaK6 👑 Senpaii'q7`, { type:'WATCHING' })
   
   console.log("CodeWork Akıyor!!")
 });
