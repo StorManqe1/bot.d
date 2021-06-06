5b7202418755499229e071d92326009450f3eabc
Glitch (v12-kayit-altyapi) <none>
2021-06-06 16:37:51 +0000

    🏫🌺 Checkpoint ./komutlar/duyu.js:32068736/946 ./komutlar/yardımmoderasyon.js:32068736/323 ./komutlar/yardımkoruma.js:32068736/36 ./komutlar/eğlenceyardım.js:32068736/106

--- komutlar/yardımmoderasyon.js
+++ komutlar/yardımmoderasyon.js
@@ -13,19 +13,21 @@ const mhelp = new Discord.MessageEmbed()
 
 
 
-<:boruto:849713659264368731>**b!ban** : İstediğiniz Kişiyi Banlar
+<:boruto:849713659264368731>**!ban** : İstediğiniz Kişiyi Banlar
 
-<:boruto:849713659264368731> **b!küfürengel** : Sunucuda Küfür Etmenize İzin Vermez
+<:boruto:849713659264368731> **!küfürengel** : Sunucuda Küfür Etmenize İzin Vermez
 
-<:boruto:849713659264368731> **b!otorol** : Otorol Mesajı Ve Otorol Ayarlar
+<:boruto:849713659264368731> **!otorol** : Otorol Mesajı Ve Otorol Ayarlar
 
-<:boruto:849713659264368731> **b!nuke** : Kanalı Siler Tekrardan Açar
+<:boruto:849713659264368731> **!nuke** : Kanalı Siler Tekrardan Açar
 
-<:boruto:849713659264368731> **b!sa-as** : Sa Mesajına Cevap Verir
+<:boruto:849713659264368731> **!sa-as** : Sa Mesajına Cevap Verir
 
-<:boruto:849713659264368731> **b!yetkilerim** : Sunucudaki Yetkilerinizi Gösterir
+<:boruto:849713659264368731> **!yetkilerim** : Sunucudaki Yetkilerinizi Gösterir
 
-<:boruto:849713659264368731> **b!sil** : 1/1000 Arası Msg Siler
+<:boruto:849713659264368731> **!sil** : 1/1000 Arası Msg Siler
+
+<:boruto:849713659264368731> **!say** : Sunucunuzda Kaç Kişi Kaç Kanal Olduğunu Söyler
 
 
 
@@ -59,7 +61,7 @@ const mhelp = new Discord.MessageEmbed()
 
 `) 
        .setImage("")
-       .addField(`»  Bot Bağlantıları`, `  [Bot Davet Linki](https://discord.com/api/oauth2/authorize?client_id=849558767845638145&permissions=8&scope=bot) `)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz 
+       .addField(`»  Bot Bağlantıları`, `  [Bot Davet Linki](https://discord.com/api/oauth2/authorize?client_id=851094764127911936&permissions=8&scope=bot) `)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz 
         .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
 message.channel.send(mhelp)
 .then; const sembed = new Discord.MessageEmbed()
