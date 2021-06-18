6ec37f02b19b7e7bd938dc1edb2fa509801a273d
Glitch (heartbreaking-opposite-pickle) <none>
2021-06-18 22:26:20 +0000

    🍞🚐 Checkpoint ./komutlar/say.js:31503053/256

--- komutlar/say.js
+++ komutlar/say.js
@@ -13,12 +13,12 @@ exports.run = async (client, message, args) => {
     const devtr  = new Discord.MessageEmbed() //#DevTR - xFalcon
         .setColor("YELLOW") //#DevTR - xFalcon
         .setThumbnail(client.user.avatarURL()) //#DevTR - xFalcon //#DevTR - xFalcon //#DevTR - xFalcon //#DevTR - xFalcon //#DevTR - xFalcon //#DevTR - xFalcon //#DevTR - xFalcon
-        .addField(`<a:kelebek5:849710939121123349> **Sunucudaki Toplam Üye Sayısı**`,`**\`\`\`${message.guild.memberCount}\`\`\`**`) //#DevTR - xFalcon
-        .addField(`<a:kelebek5:849710939121123349> **Toplam Çevrimiçi Üye Sayısı**`, `**\`\`\`${devtr_kod}\`\`\`**`) //#DevTR - xFalcon
-        .addField(`<a:kelebek5:849710939121123349>**Seslideki Üye Sayısı**`,`**\`\`\`${count}\`\`\`**`) //#DevTR - xFalcon
-        .addField(`<a:kelebek5:849710939121123349>  **Yazı Kanalları**`, `» **${textChannels}**`) //#DevTR - xFalcon
-        .addField(`<a:kelebek5:849710939121123349>  **Ses Kanalları**`, `» **${voiceChannels.size}**`) //#DevTR - xFalcon
-        .addField(`<a:kelebek4:849711002236223558> **Kullanıcılar** <a:kelebek4:849711002236223558>`, `<a:kelebek4:849711002236223558> Çevrimiçi : **${message.guild.members.cache.filter(o => o.presence.status === 'online').size}** \n<a:kelebek4:849711002236223558> Rahatsız Etmeyin : **${message.guild.members.cache.filter(dnd => dnd.presence.status === 'dnd').size}** \n<a:kelebek4:849711002236223558> Boşta: **${message.guild.members.cache.filter(i => i.presence.status === 'idle').size}** \n<a:kelebek4:849711002236223558> Görünmez/Çevrimdışı : **${message.guild.members.cache.filter(off => off.presence.status === 'offline').size}** \n<a:kelebek4:849711002236223558>  Botlar : **${botlar}**`, true) //#DevTR - xFalcon
+        .addField(`≾ **Sunucudaki Toplam Üye Sayısı**`,`**\`\`\`${message.guild.memberCount}\`\`\`**`) //#DevTR - xFalcon
+        .addField(`≾ **Toplam Çevrimiçi Üye Sayısı**`, `**\`\`\`${devtr_kod}\`\`\`**`) //#DevTR - xFalcon
+        .addField(`≾**Seslideki Üye Sayısı**`,`**\`\`\`${count}\`\`\`**`) //#DevTR - xFalcon
+        .addField(`≾  **Yazı Kanalları**`, `» **${textChannels}**`) //#DevTR - xFalcon
+        .addField(`≾  **Ses Kanalları**`, `» **${voiceChannels.size}**`) //#DevTR - xFalcon
+        .addField(`≾ **Kullanıcılar** ≾`, `≾ Çevrimiçi : **${message.guild.members.cache.filter(o => o.presence.status === 'online').size}** \n<a:kelebek4:849711002236223558> Rahatsız Etmeyin : **${message.guild.members.cache.filter(dnd => dnd.presence.status === 'dnd').size}** \n<a:kelebek4:849711002236223558> Boşta: **${message.guild.members.cache.filter(i => i.presence.status === 'idle').size}** \n<a:kelebek4:849711002236223558> Görünmez/Çevrimdışı : **${message.guild.members.cache.filter(off => off.presence.status === 'offline').size}** \n<a:kelebek4:849711002236223558>  Botlar : **${botlar}**`, true) //#DevTR - xFalcon
         .setTitle(`${message.author.tag} - Tarafından Kullanıldı`) //#DevTR - xFalcon
         .setFooter(`Ravex Bot İksdi`, client.user.avatarURL()) //#DevTR - xFalcon
     message.channel.send(devtr); //#DevTR - xFalcon
